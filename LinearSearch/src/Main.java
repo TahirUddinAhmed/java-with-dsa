0e14a6ec3d520401d068b20c0ea62bbf2c74f8db
Tahir Ahmed <01tahirahmed@gmail.com>
2023-09-19 12:09:28 +0530

    Add a modified linear search function

--- LinearSearch/src/Main.java
+++ LinearSearch/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 12, 14, 8, 43, 99, 72};
-        System.out.println(linearSearch(numbers, 13));
+        System.out.println(linearSearch(numbers, 14));
+
+
+        System.out.println(ModifiedLSearch(numbers, 13));
     }
     // linear search - return the index if item found
     // otherwise - return -1 if it is not found
@@ -19,6 +22,22 @@ public class Main {
         return found;
     }
 
-    
+    // modified linearsearch function
+    static int ModifiedLSearch(int[] arr, int target) {
+        // check if arr is empty
+        if(arr.length == 0) {
+            return -1;
+        }
+
+        // search
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return i;
+            }
+        }
+
+        // if none the statement is execute the line will run
+        return -1;
+    }
 
 }
