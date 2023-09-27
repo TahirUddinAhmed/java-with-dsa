51e9bef02959983d96096a29ff12b905aa0aa3c0
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2023-09-27 08:03:01 +0530

    First and last index

---
+++ BinarySearch/src/Questions/FirstAndLastNumber.java
@@ -0,0 +1,57 @@
+package Questions;
+
+import java.util.Arrays;
+
+public class FirstAndLastNumber {
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,8,8,10};
+        int target = 11;
+        // output = [1, 2]
+        int[] ans = searchRange(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] searchRange(int[] nums, int target) {
+      int[] ans = {-1, -1};
+
+      int start = search(nums, target, true);
+      int end = search(nums, target,false);
+
+      ans[0] = start;
+      ans[1] = end;
+
+      return ans;
+    }
+
+    static int search(int[] arr, int target, boolean findFirstIndex) {
+        int ans = -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        // binary search
+        while (start <= end) {
+            // find the mid element
+            int mid = start + (end - start);
+
+            // got the mid element - check
+            if (arr[mid] < target) {
+                // search in right
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                // initial ans found
+                ans = mid;
+                if(findFirstIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+            }
+
+        }
+
+        return ans;
+    }
+}
