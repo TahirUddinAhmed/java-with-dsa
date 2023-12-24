0c55d08185c1b543800667efcecbd2a7a3421a3d
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2023-12-24 14:03:30 +0530

    import statement

---
+++ OOP/src/tahir/com/Message.java
@@ -0,0 +1,10 @@
+package tahir.com;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+    public static void message() {
+        System.out.println("This is a personal message");
+    }
+}
--- OOP/src/tahir/com/swap.java
+++ OOP/src/tahir/com/swap.java
@@ -1,10 +1,16 @@
 package tahir.com;
 
+import static tahir.com.Message.message;
+
 public class swap {
     public static void main(String[] args) {
         int a = 5;
         int b = 7;
         swap(a, b);
+
+        message();
+
+
     }
 
     static void swap(int a, int b) {
